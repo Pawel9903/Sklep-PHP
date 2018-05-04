e3cc6d8007be31ebe2a36734d8bd63a1a57ad28e
Pawel9903 <pawel9903@wp.pl>
2018-05-04 13:52:08 +0200

    Wyświetlanie wszystkich ofert

--- classes/Announcement.php
+++ classes/Announcement.php
@@ -8,5 +8,29 @@
 
 class Announcement
 {
+    private $announcement;
+    private $pdo;
 
+    public function __construct()
+    {
+       $this->pdo = Db::getInstance()->getPdo();
+    }
+
+    public function getAnnouncementById($id)
+    {
+        $this->announcement = $this->pdo->query("SELECT * FROM announcement AS a WHERE a.id_user = '$id'")->fetchAll();
+    }
+
+    public function getAllAnnouncements()
+    {
+        $this->announcement = $this->pdo->query("SELECT a.name, a.price, a.description, a.add_date, a.end, a.type, u.user_name, u.surname, u.email FROM announcement AS a, users AS u")->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAnnouncement()
+    {
+        return $this->announcement;
+    }
 }
--- views/announcement.php
+++ views/announcement.php
@@ -1,4 +1,10 @@
 <?php
+
+spl_autoload_register(function($class)
+{
+    include '../classes/'.$class.'.php';
+});
+
 session_start();
 if(empty($_SESSION['id']))
 {
@@ -7,3 +13,31 @@ if(empty($_SESSION['id']))
 {
     echo $_SESSION['id'];
 }
+
+$announcement = new Announcement();
+$announcement->getAllAnnouncements();
+$items = $announcement->getAnnouncement();
+?>
+
+<?php include ('templates/head.php'); ?>
+
+
+<div class="container text-center">
+        <div class="row">
+            <?php foreach ($items as $item):?>
+                <div class="card col-md-6 offset-3 my-3">
+                    <div class="card-title">Nazwa: <?php echo $item['name'] ?></div>
+                    <div class="card-text">Cena: <?php echo $item['price'] ?> zł</div>
+                    <div class="card-text">Opis: <?php echo $item['description'] ?></div>
+                    <div class="card-text">Właściciel ogłoszenia</div>
+                    <div class="card-text">Imię: <?php echo $item['user_name'] ?></div>
+                    <div class="card-text">Nazwisko: <?php echo $item['surname'] ?></div>
+                    <div class="card-text">Email: <?php echo $item['email'] ?></div>
+                    <div class="card-text">Data dodania: <?php echo $item['add_date'] ?></div>
+                    <div class="card-text">Koniec Ogłoszenia: <?php echo $item['end'] ?></div>
+                    <div class="card-text">Typ: <?php echo $item['type'] ?></div>
+                </div>
+            <?php endforeach; ?>
+        </div>
+</div>
+
